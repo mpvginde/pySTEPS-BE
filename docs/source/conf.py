a38116fec8606352c8d223e97930e8770c2449f3
mpvginde <81358394+mpvginde@users.noreply.github.com>
2022-04-25 09:55:55 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'RMI-PYSTEPS'
+copyright = '2022, Royal Meteorological Institute Belgium'
+author = 'Michiel Van Ginderachter'
 
 release = '0.1'
 version = '0.1.0'
